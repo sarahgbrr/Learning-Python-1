ae6bf314d73c6f5ffe000578af3e3910e3c5f06b
Sarah Barroso <89813672+sarahgbrr@users.noreply.github.com>
2021-11-19 16:45:50 -0300

    Add files via upload

---
+++ Desafio 2.py
@@ -0,0 +1,4 @@
+dia=input('Qual é o dia do seu nascimento?')
+mes=input('Qual é o mês do seu nascimento?')
+ano=input('Qual é o seu ano de nascimento?')
+print('Você nasceu no dia',dia,'do mês',mes,'no ano de',ano,'. Muito legal!')
---
+++ Desafio 3.py
@@ -0,0 +1,4 @@
+numero1=int(input('Insira um número'))
+numero2=int(input('Insira outro número'))
+print('a soma desses números é igual a:')
+print(numero1+numero2)
